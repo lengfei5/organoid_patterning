b0763e7f45988f404247f3f79bf179e57e5a70f6
jiwang <>
2021-04-14 11:42:41 +0200

    finished first parameter extraction

--- scripts/3DImage_downstream_analysis.R
+++ scripts/3DImage_downstream_analysis.R
@@ -14,17 +14,90 @@
 ##########################################################################
 ##########################################################################
 
-rm(list=ls())
+##########################################
+# functions 
+##########################################
+cat.image.parameters.per.condition = function(files.surface)
+{
+  # files.surface = files.cyst;
+  res.cc = c()
+    
+  for(m in 1:length(files.surface))
+  {
+    # m = 1
+    name.param = basename(files.surface[m])
+    #cat(name.param, ' -- ')
+    param = read.csv(file = files.surface[m], sep = ',', header = FALSE, comment.char = '=', stringsAsFactors = FALSE)
+    kk = which(param[,1] != ' ')
+    
+    name.param = gsub('.csv', '', name.param)
+    name.param = gsub(' ', '.', name.param)
+    name.param = gsub('=', '', name.param)
+    name.param = gsub('Surfaces_[1-9]_', '', name.param)
+    name.param = gsub('_', '.', name.param)
+    
+    cat(conds[n],  ' : m = ', m, ' param.name :',  name.param, '\n')
+    
+    # colnames of extracted parameterse
+    names = as.character(unlist(param[kk[2], ]))
+    names = gsub('_', '.', names)
+    names = gsub(' ', '.', names)
+    
+    param = data.frame(param[(kk[2]+1):nrow(param), ])
+    colnames(param) = names
+    index.id = which(names == 'ID')
+    
+    if(length(index.id) != 1) {
+      stop('Error : no ID column found !!! \n')
+      
+    }else{
+      # select columns before the ID column
+      param = param[, c(index.id, 1:(index.id-1))]
+      
+      # select again the columns before Unit if it exits in the table
+      index.unit = grep('Unit', colnames(param))
+     
+      if(length(index.unit) >= 1){
+        index.unit = max(index.unit)
+        unit.uniq = unique(as.character(param[, index.unit]))
+        unit.uniq = gsub('[\\^]', '', unit.uniq)
+        if(length(unit.uniq) == 1) {
+          param = param[, c(1:(index.unit-1))]
+          
+          colnames(param)[2:ncol(param)] = paste0(colnames(param)[2:ncol(param)], '.Unit.', unit.uniq)
+          
+        }else{
+          param = param[, c(1:index.unit)]
+        }
+      }
+      
+      colnames(param)[2:ncol(param)] = paste0(colnames(param)[2:ncol(param)], '_', name.param)
+      
+      if(m == 1 ){
+        #colnames(param)[2:ncol(param)] = paste0(colnames(param)[2:ncol(param)], '_', name.param)
+        res.cc = data.frame(param, stringsAsFactors = FALSE)
+        
+      }else{
+        # double check the ID match
+        if(length(intersect(res.cc$ID, param$ID)) != length(res.cc$ID)) { cat('ID does not match exactly \n') }
+        ii = match(res.cc$ID, param$ID)
+        if(ncol(param) > 2){
+          res.cc = data.frame(res.cc, param[ii, -1], stringsAsFactors = FALSE)  
+        }
+        if(ncol(param) == 2){
+          res.cc = data.frame(res.cc, param[ii, -1], stringsAsFactors = FALSE)
+          colnames(res.cc)[ncol(res.cc)] = colnames(param)[2]
+        }
+      }
+      
+    }
+    
+  }
+  
+  return(res.cc)
+  
+}
 
-# specific input and output folders
-dataDir = '../outputs/210217_hNTdrugs3_0310outputs'
-resDir = 'results'
-analysis.verison = '_test20210410'
-
-cyst.channel = '1'
-floorplat.channel = '2'
-
-if(!dir.exists(resDir)) dir.create(resDir)
 
 ########################################################
 ########################################################
@@ -32,41 +105,17 @@ if(!dir.exists(resDir)) dir.create(resDir)
 #  
 ########################################################
 ########################################################
+# rm(list=ls())
 
-##########################################
-# functions 
-##########################################
-cat.image.parameters.per.condition = function(file.surface)
-{
-  name.param = basename(files.cyst[m])
-  #cat(name.param, ' -- ')
-  param = read.csv(file = files.cyst[m], sep = ',', header = FALSE, comment.char = '=', stringsAsFactors = FALSE)
-  kk = which(param[,1] != ' ')
-  
-  name.param = gsub('.csv', '', name.param)
-  name.param = gsub(' ', '.', name.param)
-  name.param = gsub('=', '', name.param)
-  cat(conds[n],  'm = ', m, ' param.name :',  name.param, '\n')
-  
-  names = as.character(unlist(param[kk[2], ]))
-  param = data.frame(param[(kk[2]+1):nrow(param), ])
-  colnames(param) = names
-  index.id = which(names == 'ID')
-  
-  if(length(index.id) != 1) {
-    cat('Error : no ID column found !!! \n')
-  }else{
-    param = param[, c(index.id, 1:(index.id-1))]
-    index.unit = grep('Unit', colnames(param))
-    if(length(index.unit) >= 1){
-      index.unit = max(index.unit)
-      param = param[, c(1:index.unit)]
-    }
-    
-    colnames(param)[1:ncol(param)] = paste0(colnames(param)[1:ncol(param)], '_', name.param)
-    
-  }
-}
+# specific input and output folders
+dataDir = '../210217_hNTdrugs3_0310outputs'
+resDir = '../results'
+analysis.verison = 'hNTdrugs3_0310_Analysis20210410'
+
+cyst.channel = '1'
+floorplat.channel = '2'
+
+if(!dir.exists(resDir)) dir.create(resDir)
 
 
 ##########################################
@@ -76,29 +125,29 @@ conditions.list = dir(path = dataDir, pattern = '*_Statistics', full.names = TRU
 conds = basename(conditions.list)
 conds = gsub('_Statistics', '', conds)
 
-res = c()
+
 for(n in 1:length(conds))
 {
   # n = 1
   cat(n, ' -- start ', conds[n], '\n')
+  
   files.cyst = list.files(path = paste0(conditions.list[n], '/Surfaces_', cyst.channel,   '_Statistics'), 
                           pattern = '*.csv', full.names = TRUE)
+  files.cyst =  files.cyst[grep('_Overall', files.cyst, invert = TRUE)]
   
-  res.cc = c()
-  for(m in 1:length(files.cyst))
-  {
-    # m = 1
-    if(m == 1){
-      colnames(param)[2:ncol(param)] = paste0(colnames(param)[2:ncol(param)], '_', name.param)
-      res.cc = data.frame(param, stringsAsFactors = FALSE)
-      
-    }else{
-      
-      ii = match(res.cc$ID, param$ID)
-      res.cc = data.frame(res.cc, param[ii, ])  
-    }
-    
-  }
+  res1 = cat.image.parameters.per.condition(files.cyst)
+  
+  files.fp = list.files(path = paste0(conditions.list[n], '/Surfaces_', floorplat.channel,   '_Statistics'), 
+                        pattern = '*.csv', full.names = TRUE)
+  files.fp =  files.fp[grep('_Overall', files.fp, invert = TRUE)]
+  
+  res2 = cat.image.parameters.per.condition(files.fp)
+  
+  write.table(res1, file = paste0(resDir, '/', analysis.verison, '_condition_', conds[n], '_cyst.txt'), 
+              sep = '\t', quote = FALSE, col.names = TRUE, row.names = FALSE)
+  write.table(res2, file = paste0(resDir, '/', analysis.verison, '_condition_', conds[n], '_floorplate.txt'), 
+              sep = '\t', quote = FALSE, col.names = TRUE, row.names = FALSE)
+  
   
 }
 
