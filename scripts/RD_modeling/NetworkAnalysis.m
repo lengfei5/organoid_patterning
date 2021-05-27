6c0ae0f74cc958af9e92ef2f606e491049cf25c7
jiwang <jingkui.wang@imp.ac.at>
2021-05-27 14:20:07 +0200

    check the steady stat part of matlab code

--- scripts/RD_modeling/NetworkAnalysis.m
+++ scripts/RD_modeling/NetworkAnalysis.m
@@ -1,39 +1,52 @@
 %Run SystemDefinition_example first
 
+%%
 %__________________________________________________________________________
 %ODE optimisation parameters
 options = optimoptions('fsolve','FunctionTolerance', 1.0e-12,'Display','off'); %ODE solver setting see, Matlab documentation for further details
 t_final = 1000; %Time threshold for ODE simulation
 %__________________________________________________________________________
-evaluate_diffusion = 1; %Set to 1 if Jacobian should be evaluated with diffusion. If set to 0 only the Jacobian excluding diffusion will be evaluated and no Turing instabilities will be analysed.
+evaluate_diffusion = 1; %Set to 1 if Jacobian should be evaluated with diffusion. 
+% If set to 0 only the Jacobian excluding diffusion will be evaluated and no Turing instabilities will be analysed.
+
+%%
 %__________________________________________________________________________
 %Initialise some matrices
 y0 = rand.*ones(n, 1); %Initial conditions for first simulation     
 total = length(k_grid(1,:)); %Calculates number of iterations needed to sample all parameter combinations
 z = k_length+2; 
-System_save = zeros(total,z); %System save is a matrix to save all parameters as well as information to the amount of Steady States found for each parameter set and the classfication of the system according to 0,1,2,3 (Stable,unstable,oscillating,damped oscillating). The behavior is estimated from the simulation trajectories.
+System_save = zeros(total,z); %System save is a matrix to save all parameters as well as information to the amount of Steady States 
+%found for each parameter set and the classfication of the system according to 0(Stable), 2(unstable), 3(oscillating), 4(damped oscillating). 
+%The behavior is estimated from the simulation trajectories.
 SteadyState_save = zeros(2*total,n+3); %This is a matrix that saves all steady states indexed to each parameter set.
-Turing_save = zeros(2*total,n+3); %A matrix to save which parameter combination and corresponding steady state form Turing Instabilities. In addition, the diffusion values, and what type of Turing Instability is present, are defined.
+Turing_save = zeros(2*total,n+3); %A matrix to save which parameter combination and corresponding steady state form Turing Instabilities. 
+% In addition, the diffusion values, and what type of Turing Instability is present, are defined.
 tspan = [0 t_final]; %Vector defining time for solving ODE equation using the Matlab ODE suite
+
 count_i = 1;
 len = length(c_ini(:,1));
 Saver=zeros(len^n,n);
 counter = 2+n+k_length; 
 final = counter;
+
+%%
 %__________________________________________________________________________
 %Workflow Part 1: Find steady states numerically
-
 tic;
 
-for i = 1:length(k_grid(1,:))
+for i = 1:length(k_grid(1,:)) % loop over the sampled parameters 
     k = k_grid(:,i)'; %Select k's to be sampled from k_grid  
     try [t1, xout1] = ode15s(@(t, x)ode(t,x,f_ode,k),tspan,y0); %Run first ODE simulation
     catch
         [t1, xout1] = ode23s(@(t, x)ode(t,x,f_ode,k),tspan,y0);
     end
+    
+    % figure; plot(t1,xout1(:, 1), t1, xout1(:, 2))
     ss2 = xout1(length(xout1(:,1)),:); %Suggest steady state solution from simulation
     m = BurnInCorrection(t1,xout1); %Calculate threshold for local maxima estimation
+    
     state = 0;
+    
     %Check if system is showing any oscillations:
     max_min_count = 0;
     for p1 = 1:n
@@ -44,20 +57,20 @@ for i = 1:length(k_grid(1,:))
             local_max_save = local_max;
         end 
     end
-    %Distinguish damped oscillator from oscillator using the ODE
-    %trajectories
+    
+    %Distinguish damped oscillator from oscillator using the ODE trajectories
     if max_min_count > 0
         state = OscVsDamped(local_max_save(length(local_max_save)-1),local_max_save(length(local_max_save)));
         ss = median(xout1(:,:));
     end
     %Evaluate if steady state guess is correct for non-oscillating systems
-
+    
     %Check for negative and imaginary solution and exclude
     ss2(ss2<=0)=0;
     if all(ss2) == 0 || isreal(ss2) < 1
       state = 1; 
     end
-
+    
     if state == 0 %For systems with no oscillatory behaviour and that have been found to be stable.
         %Checking for multiple steady states simulating several ODE
         %trajectories for all initial conditions defined in c_ini
@@ -67,13 +80,15 @@ for i = 1:length(k_grid(1,:))
         if isempty(C) == 1
             state = 1;
         end
+        
         %Save all non-redundant steady states
         for p1 = 1:length(C(:,1))
             System_save(i,length(k)+2)=length(C(:,1)); %save number of steady states (e.g. 2 = 2 steady states)
             SteadyState_save(count_i,1) = i;
             SteadyState_save(count_i,2:n+1)=C(p1,:); %save steady state (steady state values for each node)
             count_i = count_i + 1;
-        end    
+        end
+        
     end
 
     if state == 2 %System is found to be an oscillator and will not be further analysed for any Turing Instabilities
@@ -117,13 +132,12 @@ end
 
 SteadyState_save = SteadyState_save(1:j,:);
 
-%Time Process 1: 
 toc
 
-
+%% Workflow Part II:
 %__________________________________________________________________________
-%Workflow Part 2: Given all steady states that are either stable or damped 
-%oscillators the Jacobian eigenvalues are calculated without diffusion. 
+% Given all steady states that are either stable or damped 
+% oscillators the Jacobian eigenvalues are calculated without diffusion. 
 %For all systems where maximum(real(eigenvalue)) < 0 the eigenvalues for the
 %Jacobian including diffusion are then analysed. If any
 %maximum(real(eigenvalue)) > 0 the system exhibits a Turing I instability.
@@ -134,7 +148,6 @@ tic %Timing process 2
 
 
 % here is the symbolic part making trouble
-
 X = sym('x', [1 n]); %Create symbolic nodes x1...xn
 K = sym('k',[1 k_length]); %Create symbolic parameters k1...kn
 f_sym = f_ode(X,K); %Create symbolic function of f_ode
@@ -194,7 +207,7 @@ end
 
 toc
 
-%Delete all zero entries
+%% Delete all zero entries and save 
 for i = 1:length(Turing_save(:,1))
     if Turing_save(i,1) ==0 
         j = i-1;
