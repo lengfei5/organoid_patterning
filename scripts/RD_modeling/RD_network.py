0fbb3aae15af00edcf0870087c11cf6661f31f66
jiwang <jingkui.wang@imp.ac.at>
2021-05-28 16:17:43 +0200

    start python code to rewrite the RD topology scanning code

---
+++ scripts/RD_modeling/RD_network.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 28 16:14:06 2021
+
+The caltech course http://be150.caltech.edu/2020/content/lessons/20_turing.html
+was much helped
+
+@author: jingkui.wang
+"""
+
+import numpy as np
+import pandas as pd
+import numba
+import scipy.integrate
+
+import biocircuits
+
+import bokeh.io
+import bokeh.plotting
+
+import panel as pn
+pn.extension()
+
+
+
