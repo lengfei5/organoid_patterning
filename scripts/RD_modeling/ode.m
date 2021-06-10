d588c1337920c42161241724d451d0bb6f67dc73
jiwang <jingkui.wang@imp.ac.at>
2021-06-10 09:53:09 +0200

    remove the matlab code from tracking

--- scripts/RD_modeling/ode.m
+++
@@ -1,6 +0,0 @@
-function dxdt=ode(~, x, f, k)
-%Simple function that provides necessary inputs for running the ode solvers
-%ode15s, ode23 and ode45.
-%The function and parameter values need to be provided by the user (f, k)
-dxdt = f(x,k);
-end
