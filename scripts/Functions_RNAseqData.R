c1bf10654110e28c78ed98de6ce0edde62d2132b
jiwang <jingkui.wang@imp.ac.at>
2021-07-16 09:43:12 +0200

    add scripts/Functions_RNAseqData.R

---
+++ scripts/Functions_RNAseqData.R
@@ -0,0 +1,9 @@
+##########################################################################
+##########################################################################
+# Project: NT organoid patterning 
+# Script purpose: a collection of functions used for Sequencing data analysis
+# Usage example: 
+# Author: Jingkui Wang (jingkui.wang@imp.ac.at)
+# Date of creation: Fri Jul 16 09:42:08 2021
+##########################################################################
+##########################################################################
